ebae34599ed21a384447e9b103f829efc89e08ee
Sam Sully <sam@planetaryprocessing.io>
2025-05-21 10:08:04 +0200

    Revert "backdate to 2021, revert to sockets"
    This reverts commit 1791febbbb14b743777494d656a08bf14185373d.

--- Runtime/PPChunk.cs
+++ Runtime/PPChunk.cs
@@ -1,31 +1,27 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Planetary
-{
+namespace Planetary {
+
     [AddComponentMenu("PP/Chunk")]
     public class PPChunk : MonoBehaviour
     {
-        // It's recommended to initialize the dictionary to avoid null issues.
         internal PPMaster Master;
         internal ulong id;
         internal long x;
         internal long y;
-        internal Dictionary<string, object> data = new Dictionary<string, object>();
+        internal Dictionary<string, object> data;
 
-        // Use 'private' explicitly unless inherited protected access is needed.
-        private void Start()
+        protected void Start()
         {
-            // Initialization logic if needed
         }
 
-        private void FixedUpdate()
+        protected void FixedUpdate()
         {
-            // Physics update logic if needed
         }
 
-        public Dictionary<string, object> GetServerData()
-        {
+        public Dictionary<string, object> GetServerData() {
             return data;
         }
     }
--- Runtime/PPMaster.cs
+++ Runtime/PPMaster.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+using UnityEditor;
+using System.Threading.Tasks;
 using System.Threading;
+using System;
 
+namespace Planetary {
 
-namespace Planetary
-{
     [AddComponentMenu("PP/Master")]
     public class PPMaster : MonoBehaviour
     {
@@ -21,38 +22,31 @@ namespace Planetary
         public uint ChunkSize;
         public bool TwoDimensions = false;
         public GameObject ServerToClientObject;
-        private bool dcAlerted = false;
+        private bool dcAlerted = false; 
 
         void Awake()
         {
-            Application.runInBackground = true;
+            Application.runInBackground = true; // to prevent websockets timing out when unfocused
 
-            foreach (GameObject pf in Prefabs)
-            {
+            foreach (GameObject pf in Prefabs) {
                 PPEntity sse = pf.GetComponent<PPEntity>();
-                if (sse == null)
-                {
+                if (sse == null) {
                     Debug.LogError("Prefab lacks Entity component", pf);
-                    continue;
                 }
                 PrefabMap[sse.Type] = pf;
             }
-
-            if (ServerToClientObject)
-            {
+            if (ServerToClientObject) {
                 var callbackComponent = ServerToClientObject.GetComponent<MonoBehaviour>();
-                sdk = new SDK(GameID, HandleChunk, (Dictionary<string, object> evt) =>
-                {
-                    callbackComponent.Invoke("ServerToClient", 0f); // Adjust if needed
-                });
-            }
-            else
-            {
+                Action<Dictionary<String, object>> eventCallback = (Action<Dictionary<String, object>>)Delegate.CreateDelegate(
+                    typeof(Action<Dictionary<String, object>>), callbackComponent, "ServerToClient");
+                
+                sdk = new SDK(GameID, HandleChunk, eventCallback);
+            } else {
                 sdk = new SDK(GameID, HandleChunk);
             }
-
             Player.GetComponent<PPEntity>().Master = this;
         }
+
 
         public void Init(string username, string password, float timeout = 5000f)
         {
@@ -60,143 +54,108 @@ namespace Planetary
             dcAlerted = false;
         }
 
-        public void Join()
-        {
-            if (!sdk.IsConnected())
-            {
+        public void Join() {
+            if (!sdk.IsConnected()) {
                 Debug.LogError("Joining failed. Client not connected to Planetary Processing servers.");
-                return;
             }
-            Console.WriteLine($"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}] join start");
             sdk.Join();
         }
 
-        private void HandleChunk(Chunk cnk)
-        {
+        private void HandleChunk(Chunk cnk) {
             if (ChunkPrefab == null) return;
-
-            if (Chunks.ContainsKey(cnk.id))
-            {
-                var ppchunk = Chunks[cnk.id].GetComponent<PPChunk>();
+            if (Chunks.ContainsKey(cnk.id)) {
+                PPChunk ppchunk = Chunks[cnk.id].GetComponent<PPChunk>();
                 ppchunk.data = cnk.data;
-            }
-            else
-            {
+            } else {
                 GameObject go = Instantiate(ChunkPrefab);
-                go.transform.position = TwoDimensions ?
-                    new Vector3(cnk.x * ChunkSize, cnk.y * ChunkSize, 0) :
-                    new Vector3(cnk.x * ChunkSize, 0, cnk.y * ChunkSize);
+                if (TwoDimensions) {
+                    go.transform.position = new Vector3(cnk.x*ChunkSize, cnk.y*ChunkSize, 0);
 
-                var ppchunk = go.GetComponent<PPChunk>();
+                } else {
+                    go.transform.position = new Vector3(cnk.x*ChunkSize, 0, cnk.y*ChunkSize);
+                }
+                PPChunk ppchunk = go.GetComponent<PPChunk>();
                 ppchunk.data = cnk.data;
                 ppchunk.id = cnk.id;
                 ppchunk.x = cnk.x;
                 ppchunk.y = cnk.y;
                 Chunks[cnk.id] = go;
             }
-
             List<ulong> toRemove = new List<ulong>();
-            foreach (var kvp in Chunks)
-            {
-                var id = kvp.Key;
-                var other = kvp.Value;
-                var ppchunk = other.GetComponent<PPChunk>();
-                if (Mathf.Abs(ppchunk.x - cnk.x) > 3 || Mathf.Abs(ppchunk.y - cnk.y) > 3)
-                {
+            foreach ((ulong id, GameObject other) in Chunks) {
+                PPChunk ppchunk = other.GetComponent<PPChunk>();
+                if (Mathf.Abs(ppchunk.x-cnk.x) > 3 || Mathf.Abs(ppchunk.y-cnk.y) > 3) {
                     Destroy(other);
                     toRemove.Add(id);
                 }
             }
-
-            foreach (ulong id in toRemove)
-            {
+            foreach (ulong id in toRemove) {
                 Chunks.Remove(id);
             }
         }
 
         void FixedUpdate()
         {
-            if (!sdk.IsConnected())
-            {
-                if (!dcAlerted)
-                {
+            if (!sdk.IsConnected()) {
+                if (!dcAlerted) {
                     Debug.LogError("Connection to server lost");
                     dcAlerted = true;
                 }
                 return;
             }
-
+            
             sdk.Update();
-
-            foreach (var kvp in sdk.entities)
-            {
-                string uuid = kvp.Key;
-                Entity e = kvp.Value;
-
-                if (!Entities.ContainsKey(uuid))
-                {
+            foreach ((string uuid, Entity e) in sdk.entities) {
+                if (!Entities.ContainsKey(uuid)) {
                     GameObject g;
-                    if (sdk.UUID == uuid)
-                    {
+                    if (sdk.UUID == uuid) { 
                         g = Player;
-                    }
-                    else if (PrefabMap.ContainsKey(e.type))
-                    {
+                    } else if (PrefabMap.ContainsKey(e.type)) {
                         g = Instantiate(PrefabMap[e.type]);
-                    }
-                    else
-                    {
+                    } else {
                         continue;
                     }
-
                     Entities[uuid] = g;
                     PPEntity ppe = g.GetComponent<PPEntity>();
                     ppe.UUID = uuid;
                     ppe.Master = this;
                 }
             }
-
+            
             List<string> toRemove = new List<string>();
-            foreach (var kvp in Entities)
-            {
-                string uuid = kvp.Key;
-                GameObject e = kvp.Value;
-
-                if (!sdk.entities.ContainsKey(uuid))
-                {
-                    if (uuid != sdk.UUID)
-                    {
+            foreach ((string uuid, GameObject e) in Entities) {
+                if (!sdk.entities.ContainsKey(uuid)) {
+                    if (uuid != sdk.UUID) {
                         Destroy(e);
                     }
                     toRemove.Add(uuid);
                 }
             }
 
-            foreach (string uuid in toRemove)
-            {
+            foreach (string uuid in toRemove) {
                 Entities.Remove(uuid);
             }
         }
 
-        public Entity GetEntity(string uuid)
-        {
-            Entity result;
-            return sdk.entities.TryGetValue(uuid, out result) ? result : null;
+        public Entity GetEntity(string uuid) {
+            if (!sdk.entities.ContainsKey(uuid)) {
+                return null;
+            }
+            return sdk.entities[uuid];
         }
 
-        public List<Entity> GetEntities()
-        {
+        public List<Entity> GetEntities() {
             return new List<Entity>(sdk.entities.Values);
         }
 
-        public void Message(Dictionary<string, object> msg)
-        {
-            if (!sdk.IsConnected()) return;
+        public void Message(Dictionary<string, object> msg) {
+            if (!sdk.IsConnected()) {
+                return;
+            }
             sdk.Message(msg);
         }
 
-        public bool IsConnected()
-        {
+        public bool IsConnected(){
             return sdk.IsConnected();
         }
 
